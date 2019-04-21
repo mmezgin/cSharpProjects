2a506d8c35c9b97fd602142267494338c3781e22
mmezgin <49004855+mmezgin@users.noreply.github.com>
2019-04-22 00:07:11 +0300

    ps v2.3

---
+++ PasswordSafeApp/PasswordSafeApp/empMenu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordSafeApp
+{
+    public partial class empMenu : Form
+    {
+        public empMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            LoginPage l1 = new LoginPage();
+            this.Hide();
+            l1.Show();
+        }
+    }
+}
